d7123dbb655878363f4f6a278d919c834a41b0e9
Lorrany Sousc <llorranysousc@gmail.com>
2020-07-01 16:20:47 -0300

    Create ex06.py

---
+++ ex06.py
@@ -0,0 +1,9 @@
+'''Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique o programa para que
+ele mostre os números um ao lado do outro.'''
+
+#1
+for cont in range (1, 21):
+	print(cont)
+#2
+for cont in range (1, 21):	
+	print(cont ,end=' ')
